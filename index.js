f250dae6f72d2910c0303aea0a081ed1549650bb
henryhai <henryhaiwork@gmail.com>
2022-08-01 15:15:18 +0700

    hotfix(feature-a): add debug

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const num2 = 4;
 const hello = 'Hello world!';
 
 const addFunc = (num1, num2) => {
+  console.log('Adding');
   return num1 + num2;
 };
 
@@ -12,7 +13,7 @@ const minusFunc = (num1, num2) => {
 
 console.log(hello);
 
-console.log(`${num1} + ${num2} = ${addFunc(num1, num2)}`);
+console.log(`${num1} + ${num2} = ${addFunc(num2, num1)}`);
 
 console.log(`${num1} - ${num2} = ${minusFunc(num1, num2)}`);
 

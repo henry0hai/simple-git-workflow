de33901b054de6d8008951875106734f49f203bf
henryhai <henryhaiwork@gmail.com>
2022-08-01 14:59:13 +0700

    First commit

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const hello = "Hello world!";
+const num1 = 3;
+const num2 = 4;
+const hello = 'Hello world!';
+
+const addFunc = (num1, num2) => {
+  return num1 + num2;
+};
 
 console.log(hello);
+
+console.log(`${num1} + ${num2} = ${addFunc(num1, num2)}`);
